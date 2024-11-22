4f4676ce9e3d60ba40f07d950e861a27a453f751
itsaraphap <itsaraphap.top@gmail.com>
2024-11-22 15:26:28 +0700

    dddd

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -19,7 +19,7 @@ class Auth extends BaseController{
         if (session()->get('logged_in')) {
 
             if(session()->get('mtype') == 'member'){
-                return redirect()->to('/home/'.session()->get('mtype'));
+                return redirect()->to('/view_ecp');
             }else{
                 return redirect()->to('/main');
             }
